cf1de81dfbd3ef97fa63379306e8e1e44bb1e647
radyaadi <89497476+radyaadi@users.noreply.github.com>
2021-12-22 18:14:59 +0700

    Update model.py
    editing model var

--- model.py
+++ model.py
@@ -3,11 +3,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-crop = pd.read_csv('https://raw.githubusercontent.com/MurwanjaniTejoRiyono/capstone/master/dataset/Crop_recommendation.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/MurwanjaniTejoRiyono/capstone/master/dataset/Crop_recommendation.csv')
 
-x = crop[["N", "P", "K","temperature", "humidity", "ph", "rainfall"]]
-y = crop["label"]
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, random_state = 123, stratify = y)
+features = df[["N", "P", "K","temperature", "humidity", "ph", "rainfall"]]
+targets = df["label"]
+x_train, x_test, y_train, y_test = train_test_split(features, targets, test_size = 0.20, random_state = 123, stratify = targets)
 
 RF = RandomForestClassifier(n_estimators=150,max_depth=8,random_state=42)
 RF.fit(x_train,y_train)
